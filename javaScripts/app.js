4f3343926d51468a6de4b1798772b2eb871e4f54
AH-Aashiq <arifulhaq215@gmail.com>
2021-07-25 18:08:12 +0600

    Dark mode fix left

--- javaScripts/app.js
+++ javaScripts/app.js
@@ -1,5 +1,11 @@
 let settingsMenu = document.querySelector(".settings-menu");
+let darkBtn = document.getElementById("dark-btn");
 
 function settingsMenuToggle() {
      settingsMenu.classList.toggle("settings-menu-height");
+}
+
+darkBtn.onclick = function (){
+     darkBtn.classList.toggle("dark-btn-on");
+     document.body.classList.toggle("dark-theme");
 }
